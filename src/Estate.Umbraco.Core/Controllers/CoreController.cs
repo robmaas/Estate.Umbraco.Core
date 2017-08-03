c85b2b4511943fcda1e8b781300c0a82939fa6a7
Rob Maas <r.maas@estate.nl>
2017-08-03 16:27:47 +0200

    Initial v1.0.0.1

---
+++ src/Estate.Umbraco.Core/Controllers/CoreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace Estate.Umbraco.Core.Controllers
+{
+    public class CoreController : SurfaceController
+    {
+        public T GetCurrentItem<T>() where T : PublishedContentModel
+        {
+            return Activator.CreateInstance(typeof(T), ContextItem) as T;
+        }
+
+        public T GetItem<T>(IPublishedContent content) where T : PublishedContentModel
+        {
+            return Activator.CreateInstance(typeof(T), content) as T;
+        }
+
+        public IPublishedContent ContextItem
+        {
+            get
+            {
+                return UmbracoContext.Current.PublishedContentRequest.PublishedContent;
+            }
+        }
+    }
+}
---
+++ src/Estate.Umbraco.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Estate.Umbraco.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Estate Internet BV")]
+[assembly: AssemblyProduct("Estate.Umbraco.Core")]
+[assembly: AssemblyCopyright("Copyright © Rob Maas - Estate Internet BV 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5bfe10bb-148a-4016-9630-023b1887dc16")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
